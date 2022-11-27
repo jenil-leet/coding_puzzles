16ec0ec7bc3e37f2bb99e1ce16f1f972b57087df
jenil-leet <117168656+jenil-leet@users.noreply.github.com>
2022-11-28 03:35:47 +0530

    Time: 203 ms (44.18%), Space: 14.6 MB (81.22%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -4,4 +4,5 @@ class Solution:
         if(m*n!=r*c):
             return(mat)
         flattened = [j for i in mat for j in i]
-        return([flattened[c*i:c*(i+1)] for i in range(r)])
+        reshaped = [flattened[c*i:c*(i+1)] for i in range(r)]
+        return(reshaped)
